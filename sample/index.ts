2a8d4d6abef1a01c8cdccf9743525be1bc10a41b
Mac Ignacio <mack_ignacio@yahoo.com>
2019-08-11 14:45:50 +0800

    Add unhandleErrors()

--- sample/index.ts
+++ sample/index.ts
@@ -1,9 +1,11 @@
-import { Express } from "express";
+import { Express, Request, Response } from "express";
 import http from "http";
 import server from "./server";
 
-class App {
-  constructor(private app: Express) {}
+class Maya {
+  constructor(private app: Express) {
+    this.unhandleErrors(app);
+  }
 
   start(port?: number | string): void {
     port = this.normalizePort(port || 3333);
@@ -21,7 +23,16 @@ class App {
   onListen(port: any): void {
     console.log("\x1b[32mListening on port:", `\x1b[36m${port}\x1b[0m`);
   }
+
+  unhandleErrors(app: Express): void {
+    app.use((req: Request, res: Response) => {
+      if (!req.route) {
+        const url = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
+        res.status(405).json({ status: "Invalid Request", message: `Request: (${req.method}) ${url} is invalid!` });
+      }
+    });
+  }
 }
 
-const app = new App(server.app);
+const app = new Maya(server.app);
 app.start(process.env.PORT);
