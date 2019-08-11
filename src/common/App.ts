db947a11011a13966e75baba724e61e9cb6b13de
Mac Ignacio <mack_ignacio@yahoo.com>
2019-08-12 06:24:10 +0800

    Fix #4 - Add App decorator

--- src/common/App.ts
+++ src/common/App.ts
@@ -1,47 +1,42 @@
-import express, { NextFunction, Request, RequestHandler, Response, Router } from "express";
-import { Controllers, IAppSettings, IRoute } from "../interfaces";
-import { ErrorCallback, Callback } from "../typings";
+import express, { NextFunction, Request, Response } from "express";
+import { IAppSettings, IRoute, IRoutesOptions, IRoutes } from "../interfaces";
+import { Callback } from "../typings";
 import { Injector } from "./Injector";
 import { Models } from "../models";
 
 export function App(settings: IAppSettings): <T extends new (...args: Array<{}>) => any>(target: T) => void {
-  return <T extends new (...args: Array<{}>) => Controllers>(target: T) => {
-    return;
-  };
-}
+  const { port = 3333, cors = false, logs = "", mongoConnection } = settings;
+  return (target: any): void => {
+    const configRoutes = (args: IRoutesOptions): IRoutes => {
+      const { middlewares = [], callback = (error: any, req: Request, res: Response, next: NextFunction): void => next() } = args;
+      const router = express.Router();
 
-/**
- * Add controllers to specific route with middlewares if any
- * @param args Object { route?: string; controllers: any[]; middlewares?: any[];
- * callback?: (error: any, req: Request, res: Response, next: NextFunction) => void; }
- * @param args.route string - name of route
- * @param args.controllers Array<any> - List of controllers
- * @param args.middlewares Array<Function> - List of middlewares
- * @param args.callback Function - Callback function that last to be called
- */
-function routes(args: {
-  route?: string;
-  controllers: any[];
-  middlewares?: any[];
-  callback?: ErrorCallback;
-}): { route: any; middlewares?: any; router: Router } {
-  const { middlewares = [], callback = (error: any, req: Request, res: Response, next: NextFunction): void => next() } = args;
-  const router = express.Router();
+      args.controllers.map(controller => {
+        const instance = Injector.resolve<typeof controller>(controller);
+        const prefix: string = Reflect.getMetadata("prefix", controller);
+        const model: string = Reflect.getMetadata("model", controller);
 
-  args.controllers.map(controller => {
-    const instance = Injector.resolve<typeof controller>(controller);
-    const prefix: string = Reflect.getMetadata("prefix", controller);
-    const model: string = Reflect.getMetadata("model", controller);
+        if (model) {
+          import(model).then(e => {
+            Models.addModel({ [prefix.replace("/", "")]: e.default });
+          });
+        }
 
-    if (model) {
-      import(model).then(e => {
-        Models.addModel({ [prefix.replace("/", "")]: e.default });
+        const routes: IRoute[] = Reflect.getMetadata("routes", controller);
+        const method = (name: string): Callback => (req: Request, res: Response, next: NextFunction): void => instance[name](req, res, next);
+        routes.map(route => router[route.requestMethod](prefix + route.path, route.validations, method(route.methodName), callback));
       });
-    }
+      return { path: args.path || "", middlewares, router };
+    };
 
-    const routes: IRoute[] = Reflect.getMetadata("routes", controller);
-    const method = (name: string): Callback => (req: Request, res: Response, next: NextFunction): void => instance[name](req, res, next);
-    routes.map(route => router[route.requestMethod](prefix + route.path, route.validations, method(route.methodName), callback));
-  });
-  return { route: args.route || "", middlewares, router };
+    const routes: IRoutes[] = settings.routes.map(route => {
+      return configRoutes(route);
+    });
+
+    target.routes = routes;
+    target.port = port;
+    target.cors = cors;
+    target.logs = logs;
+    target.mongoConnection = mongoConnection;
+  };
 }
