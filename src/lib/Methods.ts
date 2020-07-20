cad4c4ae1973d44351d94e70886e2355e5587075
Mac Ignacio <mack_ignacio@yahoo.com>
2020-07-20 16:28:24 +0800

    FEAT: Remove Check decorator

--- src/lib/Methods.ts
+++ src/lib/Methods.ts
@@ -57,23 +57,3 @@ export function Delete(properties: { path: string; middlewares?: Array<(...args:
   console.log("Delete decorator is deprecated since version 0.3.0. Will be deleted in version 1.0.0. Import this decorator in @mayajs/common instead.");
   return MethodDecoratorFactory("delete")(properties);
 }
-
-/**
- * Checks a specified field for validation
- *
- * @deprecated Since version 0.3.0. Will be deleted in version 1.0.0. Import this method in `@mayajs/common` instead.
- * @param fieldName name of the field to be checked
- */
-export function Check(fieldName: string): IChain {
-  console.log("Check is deprecated since version 0.3.0. Will be deleted in version 1.0.0. Import this decorator in @mayajs/common instead.");
-  const runner = new Runner(fieldName);
-  const middleware: any = (req: Request, res: Response, next: NextFunction) => {
-    const error = runner.run(req);
-    if (error.status) {
-      res.status(403).json({ status: "Validation Error", message: error.message });
-    } else {
-      next();
-    }
-  };
-  return Object.assign(middleware, DIContainer(new Functions<IChain>(runner, middleware)));
-}
