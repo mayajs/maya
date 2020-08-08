a604d8f9841e1e47b5e9379a26c7d7665c4656da
Mac Ignacio <mack_ignacio@yahoo.com>
2020-08-09 02:11:08 +0800

    FEAT: Remove configure routes in App decorator

--- src/utils/App.ts
+++ src/utils/App.ts
@@ -7,25 +7,7 @@ export function App(settings: IAppSettings): <T extends new (...args: Array<{}>)
   const { port = 3333, cors = false, logs = "", database, databases = [] } = settings;
 
   return (target: any): void => {
-    const configRoutes = (args: IRoutesOptions): IRoutes => {
-      const { middlewares = [], callback = (error: any, req: Request, res: Response, next: NextFunction): void => next() } = args;
-      const router = express.Router();
-
-      args.controllers.map((controller: any) => {
-        const instance = Injector.resolve<typeof controller>(controller);
-        const prefix: string = Reflect.getMetadata("prefix", controller);
-        const routes: IRoute[] = Reflect.getMetadata("routes", controller);
-        const method = (name: string): Callback => (req: Request, res: Response, next: NextFunction): void => instance[name](req, res, next);
-        routes.map((route: IRoute) => router[route.requestMethod](prefix + route.path, route.middlewares, method(route.methodName), callback));
-      });
-      return { path: args.path || "", middlewares, router };
-    };
-
-    const routes: IRoutes[] = settings.routes.map((route: IRoutesOptions) => {
-      return configRoutes(route);
-    });
-
-    target.routes = routes;
+    target.routes = settings.routes;
     target.port = !isNaN(port) ? port : 3333;
     target.cors = cors;
     target.logs = logs;
