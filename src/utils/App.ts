261581bc88663a3e175e881c2d028c5228e0c510
Mack Ignacio <mack_ignacio@yahoo.com>
2020-11-27 22:12:11 +0800

    FEAT: Add plugins in target object

--- src/utils/App.ts
+++ src/utils/App.ts
@@ -1,14 +1,14 @@
 import { IAppSettings } from "../interfaces";
 
 export function App(settings: IAppSettings): <T extends new (...args: Array<{}>) => any>(target: T) => void {
-  const { port = 3333, cors = false, logs = "", database, databases = [] } = settings;
+  const { port = 3333, cors = false, logs = "", plugins = [], databases = [] } = settings;
 
   return (target: any): void => {
     target.routes = settings.routes;
     target.port = !isNaN(port) ? port : 3333;
     target.cors = cors;
     target.logs = logs;
-    target.database = database;
+    target.plugins = plugins;
     target.databases = databases;
   };
 }
