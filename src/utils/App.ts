4b9aae1fc70978b59322d860acd9cf5a45383d0f
Mack Ignacio <mack_ignacio@yahoo.com>
2020-11-27 23:13:09 +0800

    FEAT: Remove unused variables in App decorator

--- src/utils/App.ts
+++ src/utils/App.ts
@@ -1,14 +1,10 @@
 import { IAppSettings } from "../interfaces";
 
 export function App(settings: IAppSettings): <T extends new (...args: Array<{}>) => any>(target: T) => void {
-  const { port = 3333, cors = false, logs = "", plugins = [], databases = [] } = settings;
+  const { databases = [], routes = [] } = settings;
 
   return (target: any): void => {
-    target.routes = settings.routes;
-    target.port = !isNaN(port) ? port : 3333;
-    target.cors = cors;
-    target.logs = logs;
-    target.plugins = plugins;
+    target.routes = routes;
     target.databases = databases;
   };
 }
