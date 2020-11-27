9854d3ae0416712eb12247f75e7dcc082a703b34
Mack Ignacio <mack_ignacio@yahoo.com>
2020-11-27 21:11:47 +0800

    FEAT: Remove unused imports

--- src/utils/App.ts
+++ src/utils/App.ts
@@ -1,7 +1,4 @@
-import express, { NextFunction, Request, Response } from "express";
-import { IAppSettings, IRoute, IRoutesOptions, IRoutes } from "../interfaces";
-import { Callback } from "../types";
-import { Injector } from "./Injector";
+import { IAppSettings } from "../interfaces";
 
 export function App(settings: IAppSettings): <T extends new (...args: Array<{}>) => any>(target: T) => void {
   const { port = 3333, cors = false, logs = "", database, databases = [] } = settings;
