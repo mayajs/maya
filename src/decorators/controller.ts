3dd096c94d17e52d9f70ebf7b1b40031b584c51b
Mack Ignacio <mack_ignacio@yahoo.com>
2020-11-29 20:47:52 +0800

    FEAT: User variable constant instead of static strings

--- src/decorators/controller.ts
+++ src/decorators/controller.ts
@@ -1,15 +1,16 @@
+import { CONTROLLER_NAME, CONTROLLER_ROUTES } from "../utils";
+
 /**
  * Decorator for a controller of a route
  * @param route name of the controller that will be added on the route name
  */
 export function Controller({ selector }: { selector: string }): ClassDecorator {
   return (target: any): void => {
-    target.__selector = selector;
-    Reflect.defineMetadata("prefix", selector, target);
+    Reflect.defineMetadata(CONTROLLER_NAME, selector, target);
 
     // Since routes are set by our methods this should almost never be true (except the controller has no methods)
-    if (!Reflect.hasMetadata("routes", target)) {
-      Reflect.defineMetadata("routes", [], target);
+    if (!Reflect.hasMetadata(CONTROLLER_ROUTES, target)) {
+      Reflect.defineMetadata(CONTROLLER_ROUTES, [], target);
     }
   };
 }
