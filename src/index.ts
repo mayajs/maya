60489260f7cd46b28bd4cd88dbe18346b623a149
Mack Ignacio <mack_ignacio@yahoo.com>
2020-11-30 08:29:12 +0800

    FEAT: Add utils constants in index exports

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export * from "./interfaces";
 export * from "./decorators";
 export * from "./di";
 export * from "./core";
+export * from "./utils/constants";
 export { Request, Response, NextFunction } from "express";
