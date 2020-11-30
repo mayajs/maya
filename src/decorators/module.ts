40266fdb2609e615df29a4be23832406505e6683
Mack Ignacio <mack_ignacio@yahoo.com>
2020-11-30 08:31:33 +0800

    FEAT: Add default module path for each module

--- src/decorators/module.ts
+++ src/decorators/module.ts
@@ -1,6 +1,6 @@
 import { ModuleOptions } from "../interfaces";
 import { ModuleOptionsKeys } from "../types";
-import { getModuleConstant } from "../utils";
+import { getModuleConstant, MODULE_PATH } from "../utils";
 
 /**
  * Decorator for MayaJS Module
@@ -9,6 +9,7 @@ import { getModuleConstant } from "../utils";
  */
 export function MOD(options: ModuleOptions): <T extends new (...args: Array<{}>) => any>(target: T) => void {
   return (target: any): void => {
+    Reflect.defineMetadata(MODULE_PATH, "", target);
     Object.keys(options).map(key => {
       const constants = getModuleConstant(`MODULE_${key.toLocaleUpperCase()}`);
       Reflect.defineMetadata(constants, options[key as ModuleOptionsKeys], target);
