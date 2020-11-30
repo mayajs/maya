290435552dfcb72fef81bdd251aa5d14136f9f4f
Mack Ignacio <mack_ignacio@yahoo.com>
2020-11-30 10:36:24 +0800

    FEAT: Add EmptyDeclarationError

--- src/exceptions/normal-exceptions.ts
+++ src/exceptions/normal-exceptions.ts
@@ -1,6 +1,11 @@
-import { DUPLICATE_DECLARATIONS } from "./message";
+import { DUPLICATE_DECLARATIONS, EMPTY_DECLARATIONS } from "./message";
 
 /**
  * Create an Error instance for duplicated declaration
  */
 export const DuplicateDeclarationError = (module: string, controller: string) => new Error(DUPLICATE_DECLARATIONS(module, controller));
+
+/**
+ * Create an Error instance for empty declaration
+ */
+export const EmptyDeclarationError = (module: string) => new Error(EMPTY_DECLARATIONS(module));
