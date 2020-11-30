4af93dc449f904b89706b9d02255409d6de504c4
Mack Ignacio <mack_ignacio@yahoo.com>
2020-11-30 11:06:24 +0800

    FEAT: Add NotFoundError exception

--- src/exceptions/normal-exceptions.ts
+++ src/exceptions/normal-exceptions.ts
@@ -1,4 +1,4 @@
-import { DUPLICATE_DECLARATIONS, EMPTY_DECLARATIONS } from "./message";
+import { DUPLICATE_DECLARATIONS, EMPTY_DECLARATIONS, NOT_FOUND } from "./message";
 
 /**
  * Create an Error instance for duplicated declaration
@@ -9,3 +9,8 @@ export const DuplicateDeclarationError = (module: string, controller: string) =>
  * Create an Error instance for empty declaration
  */
 export const EmptyDeclarationError = (module: string) => new Error(EMPTY_DECLARATIONS(module));
+
+/**
+ * Create an Error instance for not found
+ */
+export const NotFoundError = (message: string) => new Error(NOT_FOUND(message));
