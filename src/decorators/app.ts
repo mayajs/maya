a25ce1d20f7272adada13acd0199cecd373d97d0
Mack Ignacio <mack_ignacio@yahoo.com>
2020-11-30 08:42:46 +0800

    FEAT: Remove metadata for controller routes and databases

--- src/decorators/app.ts
+++ src/decorators/app.ts
@@ -1,17 +1,15 @@
-import { IAppModuleOptions } from "../interfaces";
-import { CONTROLLER_ROUTES, DATABASE, MODULE_DECLARATIONS, MODULE_EXPORTS, MODULE_IMPORTS } from "../utils";
+import { MODULE_DECLARATIONS, MODULE_EXPORTS, MODULE_IMPORTS } from "../utils";
+import { ModuleOptions } from "../interfaces";
 
 /**
  * Decorator for app module
  *
  * @param options Settings for app module
  */
-export function App(options: IAppModuleOptions): <T extends new (...args: Array<{}>) => any>(target: T) => void {
-  const { databases = [], routes = [], exports = [], imports = [], declarations = [], bootstrap } = options;
+export function App(options: ModuleOptions): <T extends new (...args: Array<{}>) => any>(target: T) => void {
+  const { exports = [], imports = [], declarations = [], bootstrap } = options;
 
   return (target: any): void => {
-    Reflect.defineMetadata(CONTROLLER_ROUTES, routes, target);
-    Reflect.defineMetadata(DATABASE, databases, target);
     Reflect.defineMetadata(MODULE_EXPORTS, exports, target);
     Reflect.defineMetadata(MODULE_IMPORTS, imports, target);
     Reflect.defineMetadata(MODULE_DECLARATIONS, declarations, target);
