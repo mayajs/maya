9754302efdb65e1c9a79a1f64d4100e675090e61
Mack Ignacio <mack_ignacio@yahoo.com>
2020-11-30 10:12:26 +0800

    FEAT: Generate unique id for each controller

--- src/decorators/controller.ts
+++ src/decorators/controller.ts
@@ -1,12 +1,16 @@
+// EXTERNAL IMPORTS
+import shortid from "shortid";
+
+// LOCAL IMPORTS
 import { CONTROLLER_NAME, CONTROLLER_ROUTES } from "../utils";
 
 /**
  * Decorator for a controller of a route
  * @param route name of the controller that will be added on the route name
  */
-export function Controller({ selector }: { selector: string }): ClassDecorator {
+export function Controller(): ClassDecorator {
   return (target: any): void => {
-    Reflect.defineMetadata(CONTROLLER_NAME, selector, target);
+    Reflect.defineMetadata(CONTROLLER_NAME, shortid.generate(), target);
 
     // Since routes are set by our methods this should almost never be true (except the controller has no methods)
     if (!Reflect.hasMetadata(CONTROLLER_ROUTES, target)) {
