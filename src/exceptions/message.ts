95e03ca77ba14208b12a752aa0ecc5a7eb1dfa57
Mack Ignacio <mack_ignacio@yahoo.com>
2020-11-30 10:36:07 +0800

    FEAT: Add EMPTY_DECLARATIONS

--- src/exceptions/message.ts
+++ src/exceptions/message.ts
@@ -2,3 +2,8 @@
  * An error message for duplicated controller declaration of a module
  */
 export const DUPLICATE_DECLARATIONS = (module: string, controller: string) => `${module} has duplicated declaration for ${controller}.`;
+
+/**
+ * An error message for empty declaration
+ */
+export const EMPTY_DECLARATIONS = (module: string) => `${module} has no declared controllers.`;
