6e959f70928fee6507000510e3cab650d0bb0f29
Mack Ignacio <mack_ignacio@yahoo.com>
2020-11-30 09:00:45 +0800

    FEAT: Define module name metadata

--- src/decorators/module.ts
+++ src/decorators/module.ts
@@ -1,6 +1,6 @@
 import { ModuleOptions } from "../interfaces";
 import { ModuleOptionsKeys } from "../types";
-import { getModuleConstant, MODULE_PATH } from "../utils";
+import { getModuleConstant, MODULE_NAME, MODULE_PATH } from "../utils";
 
 /**
  * Decorator for MayaJS Module
@@ -10,6 +10,7 @@ import { getModuleConstant, MODULE_PATH } from "../utils";
 export function Module(options: ModuleOptions): <T extends new (...args: Array<{}>) => any>(target: T) => void {
   return (target: any): void => {
     Reflect.defineMetadata(MODULE_PATH, "", target);
+    Reflect.defineMetadata(MODULE_NAME, target.name, target);
     Object.keys(options).map(key => {
       const constants = getModuleConstant(`MODULE_${key.toLocaleUpperCase()}`);
       Reflect.defineMetadata(constants, options[key as ModuleOptionsKeys], target);
