69f2657c0dc2674284603833a1b60d12b78cc3ce
Mack Ignacio <mack_ignacio@yahoo.com>
2020-11-30 11:25:51 +0800

    FEAT: Add UndeclaredDeclarationError exceptions

--- src/exceptions/normal-exceptions.ts
+++ src/exceptions/normal-exceptions.ts
@@ -1,4 +1,4 @@
-import { DUPLICATE_DECLARATIONS, EMPTY_DECLARATIONS, NOT_DECLARED, NOT_FOUND } from "./message";
+import { DUPLICATE_DECLARATIONS, EMPTY_DECLARATIONS, NOT_DECLARED, NOT_DECLARED_MODULE, NOT_FOUND } from "./message";
 
 /**
  * Create an Error instance for duplicated declaration
@@ -19,3 +19,8 @@ export const NotFoundError = (target: string) => new Error(NOT_FOUND(target));
  * Create an Error instance for undeclared components
  */
 export const NotDeclaredError = (target: string) => new Error(NOT_DECLARED(target));
+
+/**
+ * Create an Error instance for undeclared components on a module
+ */
+export const UndeclaredDeclarationError = (target: string, module: string) => new Error(NOT_DECLARED_MODULE(target, module));
