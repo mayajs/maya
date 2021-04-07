f70a2210e5a112bae0160e5eaf4b51b941ee8ea4
Mac Ignacio <mack_ignacio@yahoo.com>
2021-04-07 15:39:40 +0800

    CHORE: Remove shortid in Controller decorator

--- src/decorators/controller.ts
+++ src/decorators/controller.ts
@@ -1,8 +1,7 @@
 // EXTERNAL IMPORTS
-import shortid from "shortid";
 
 // LOCAL IMPORTS
-import { CONTROLLER_NAME, CONTROLLER_ROUTES } from "../utils";
+import { CONTROLLER_ROUTES } from "../utils";
 
 /**
  * Decorator for a controller of a route
@@ -10,8 +9,6 @@ import { CONTROLLER_NAME, CONTROLLER_ROUTES } from "../utils";
  */
 export function Controller(): ClassDecorator {
   return (target: any): void => {
-    Reflect.defineMetadata(CONTROLLER_NAME, shortid.generate(), target);
-
     // Since routes are set by our methods this should almost never be true (except the controller has no methods)
     if (!Reflect.hasMetadata(CONTROLLER_ROUTES, target)) {
       Reflect.defineMetadata(CONTROLLER_ROUTES, [], target);
