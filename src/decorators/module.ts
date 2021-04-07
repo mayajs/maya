77813c7cffcd2a77ac799f939657b04e12b668c3
Mac Ignacio <mack_ignacio@yahoo.com>
2021-04-07 15:39:12 +0800

    FEAT: Add ModuleDecoratorProps in Module decorator

--- src/decorators/module.ts
+++ src/decorators/module.ts
@@ -1,13 +1,32 @@
-import { ModuleProperty } from "../interfaces";
-import { ModuleOptionsKeys } from "../types";
 import { getModuleConstant, MODULE_NAME, MODULE_PATH } from "../utils";
+import { ModuleOptionsKeys } from "../types";
+import { Class } from "../interfaces";
+
+interface ModuleDecoratorProps {
+  bootstrap?: Class<any>;
+  declarations?: Array<Class<any>>;
+  imports?: Array<Class<any>>;
+  exports?: Array<Class<any>>;
+  providers?: Array<Class<any>>;
+}
 
 /**
  * Decorator for MayaJS Module
+
+ * ```
+ * {
+ *  port: 3333,
+ *  imports: [],
+ *  exports: [],
+ *  providers: [],
+ *  declarations: [],
+ *  bootstrap: Controller,
+ * }
+ * ```
  *
  * @param options MayaJS Module options
  */
-export function Module(options: ModuleProperty): <T extends new (...args: Array<{}>) => any>(target: T) => void {
+export function Module(options: ModuleDecoratorProps): <T extends new (...args: Array<{}>) => any>(target: T) => void {
   return (target: any): void => {
     Reflect.defineMetadata(MODULE_PATH, "", target);
     Reflect.defineMetadata(MODULE_NAME, target.name, target);
