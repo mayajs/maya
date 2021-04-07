f268b2d65f0f98816d7f5e914593aecb81cbf2fd
Mac Ignacio <mack_ignacio@yahoo.com>
2021-04-08 05:17:48 +0800

    FIX: Remove module resolving in bootstrapModule function

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,26 @@
-import { CONTROLLER_ROUTES, MODULE_BOOTSTRAP, MODULE_DECLARATIONS } from "./utils";
-import maya, { MayaJsModule, RouterModule, Type } from "@mayajs/router";
+import maya, { Type, ModuleCustomType } from "@mayajs/router";
 import "reflect-metadata";
 import http from "http";
 
-class AppModule {}
+abstract class AppModule {}
 
-export * from "./decorators/module";
-export * from "./decorators/controller";
+type BoostrapFunction = (APP_MODULE: Type<AppModule>) => void;
 
-export const configServer = (port: number) => {
-  return {
-    bootstrapModule: bootstrapModule(port),
-  };
-};
+interface MayaJsServer {
+  bootstrapModule: BoostrapFunction;
+}
+
+export * from "./decorators";
+
+export const configServer = (PORT: number): MayaJsServer => ({ bootstrapModule: bootstrapModule(PORT) });
 
 /**
  * Creates an instance of MayaJS Server on runtime.
  *
  * @param module MayaJS Module
  */
-const bootstrapModule = (port: number) => (app: Type<AppModule>) => {
-  const router = maya();
-  const controller = Reflect.getMetadata(MODULE_BOOTSTRAP, app);
-  const declarations = Reflect.getMetadata(MODULE_DECLARATIONS, app);
-  const isDeclared = declarations.some((item: any) => item.name === controller.name);
-
-  const controllerRoutes = Reflect.getMetadata(CONTROLLER_ROUTES, controller);
-  controller.prototype["routes"] = controllerRoutes;
-
-  if (!isDeclared) {
-    throw `\x1b[31m[mayajs] ${controller.name} is not properly declared on ${app.name}.\x1b[0m`;
-  }
-
-  const _module = class extends MayaJsModule {
-    imports = [RouterModule.forRoot([{ path: "", controller }])];
-    declarations = [controller];
-  };
-
-  router.add([{ path: "", loadChildren: () => Promise.resolve(_module) }]);
-  http.createServer(router).listen(port, () => console.log(`\x1b[32m[mayajs] Server is running on port ${port}\x1b[0m`));
+const bootstrapModule = (PORT: number): BoostrapFunction => (APP_MODULE: Type<AppModule>) => {
+  const MAYA = maya();
+  MAYA.add([{ path: "", loadChildren: () => Promise.resolve(<ModuleCustomType>APP_MODULE) }]);
+  http.createServer(MAYA).listen(PORT, () => console.log(`\x1b[32m[mayajs] Server is running on port ${PORT}\x1b[0m`));
 };
