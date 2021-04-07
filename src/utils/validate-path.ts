9e95d899805006c42a4738bc2177019364fadc6b
Mac Ignacio <mack_ignacio@yahoo.com>
2021-04-08 05:03:30 +0800

    CHORE: Remove validate-path.ts

--- src/utils/validate-path.ts
+++
@@ -1,3 +0,0 @@
-export function validatePath(path: string): string {
-  return path ? (path.startsWith("/") ? ("/" + path.replace(/\/+$/, "")).replace(/\/+/g, "/") : "/" + path.replace(/\/+$/, "")) : "/";
-}
